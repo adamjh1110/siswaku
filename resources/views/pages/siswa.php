eab6c281f02de726b068a8e6426f772a51615616
Adam Jamil <adamjh0788@gmail.com>
2020-01-16 07:51:29 +0700

    menambahkan routing

--- resources/views/pages/siswa.php
+++ resources/views/pages/siswa.php
@@ -1,9 +1,12 @@
 <div id="siswa">
+  <h2>Nama Kelompok: <?php echo $siswa->nama_kelompok ?></h2>
+  <h2>Jumlah murid: <?php echo $siswa->jumlah_murid ?></h2>
+  <h2>Apakah mereka bisa baca: <?php echo $siswa->bisa_baca ?></h2>
   <h2>siswa</h2>
 
-  <?php if (!empty ($siswa)):?>
+  <?php if (!empty ($siswa->daftar_murid)):?>
     <ul>
-      <?php foreach($siswa as $anak): ?>
+      <?php foreach($siswa->daftar_murid as $anak): ?>
         <li><?= $anak ?></li>
       <?php endforeach ?>
     
@@ -11,6 +14,4 @@
   <?php else: ?>
     <p> Tidak ada data siswa.</p>
   <?php endif ?>
-      
-
 </div>
